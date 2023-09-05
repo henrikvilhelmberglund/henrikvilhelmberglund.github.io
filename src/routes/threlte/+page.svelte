df3f19b027393ee805845ec35e1f77784ef41733
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-05 14:56:23 +0200

    feat: :sparkles: add Threlte version

---
+++ src/routes/threlte/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Canvas } from "@threlte/core";
+	import Scene from "./Scene.svelte";
+</script>
+
+<div class="h-screen w-screen">
+	<div class="absolute" />
+	<Canvas>
+		<Scene />
+	</Canvas>
+</div>
+
+<style>
+</style>
