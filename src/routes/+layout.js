1dc196c6cf1f5e67201592d807a14c3f8dc97df5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-10 17:06:41 +0200

    feat: :sparkles: add load function for loading translations

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,14 @@
 export const prerender = true;
+
+import { loadTranslations } from "$lib/i18n/i18n";
+
+/** @type {import('@sveltejs/kit').Load} */
+export const load = async ({ url }) => {
+	const { pathname } = url;
+
+	const initLocale = "en"; // get from cookie, user session, ...
+
+	await loadTranslations(initLocale, pathname); // keep this just before the `return`
+
+	return {};
+};
