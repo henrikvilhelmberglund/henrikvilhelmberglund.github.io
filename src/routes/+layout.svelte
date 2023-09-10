c3d62732867e489cec85598855cba8ce8b6a2164
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-10 17:07:03 +0200

    feat: :sparkles: add LanguagePicker and nav to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
+	import LanguagePicker from "$lib/components/LanguagePicker.svelte";
 	import "@unocss/reset/tailwind.css";
+
+	const routes = {
+		home: { display: "Home", url: "/" },
+		projects: { display: "Projects", url: "/projects" },
+	};
 </script>
 
+<nav class="shadow-primary-200 relative flex h-14 max-w-full w-screen shadow-lg">Home</nav>
+<nav class="absolute right-0  max-w-full top-0 justify-end">
+	<LanguagePicker />
+</nav>
 <slot />
 
 <style></style>
