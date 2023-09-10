d175ad467fc42c6834ae8d7db9afab35195b532c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-10 17:07:26 +0200

    feat: :sparkles: use i18n for the name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,26 +6,26 @@
 	import ThemeSwitcher from "$lib/theme/ThemeSwitcher.svelte";
 	import { browser } from "$app/environment";
 	import { color } from "$lib/stores";
-	import MeltAccordion from "$lib/melt-examples/MeltAccordion.svelte";
 	import H1 from "$lib/components/H1.svelte";
+	import { t } from "$lib/i18n/i18n";
+	import H2 from "$lib/components/H2.svelte";
 
 	// let color = "blue";
 	// $: if (browser) color = localStorage.color;
 </script>
 
 <main
-	class="dark:bg-primary-950 bg-primary-100 min-w-screen flex min-h-screen flex-col items-center [&>*]:m-4">
+	class="dark:bg-primary-950 bg-primary-100 w-screen max-w-full flex min-h-screen flex-col items-center [&>*]:my-4">
 	<header>
 		<ThemeSwitcher />
 		<DarkModeToggle />
 	</header>
 
-	<H1>Welcome to my profile page!</H1>
+	<H1>{$t("common.name")}</H1>
+	<H2>Welcome to my profile page!</H2>
 	<p class="text-primary-900 dark:text-primary-100">This is the {$color} theme.</p>
 	<button class="btn-primary">A {$color} button</button>
 	<button class="btn-secondary">A {$color} secondary button</button>
-	<p class="text-primary-900 dark:text-primary-100">Now with Melt!</p>
-	<MeltAccordion />
 </main>
 
 <Footer />
