d8479f7df8535ab1763b3c09663bd90071075aa6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-10 17:32:38 +0200

    feat: :lipstick: add basic styling for links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,19 @@
 	import LanguagePicker from "$lib/components/LanguagePicker.svelte";
 	import "@unocss/reset/tailwind.css";
 
-	const routes = {
-		home: { display: "Home", url: "/" },
-		projects: { display: "Projects", url: "/projects" },
-	};
+	const routes = [
+		{ display: "Home", url: "/" },
+		{ display: "Projects", url: "/projects" },
+	];
 </script>
 
-<nav class="shadow-primary-200 relative flex h-14 max-w-full w-screen shadow-lg">Home</nav>
-<nav class="absolute right-0  max-w-full top-0 justify-end">
+<nav
+	class="shadow-primary-200 dark:shadow-primary-800 relative bg-white dark:bg-primary-950 flex h-14 w-screen max-w-full justify-center items-center gap-12 shadow-lg">
+	{#each routes as { url, display }}
+		<a class="hover:bg-primary-200 dark:hover:bg-primary-900 bg-white dark:bg-primary-950 text-black dark:text-white rounded bg-white p-2 text-3xl" href={url}>{display}</a>
+	{/each}
+</nav>
+<nav class="absolute right-0 top-0 max-w-full justify-end">
 	<LanguagePicker />
 </nav>
 <slot />
