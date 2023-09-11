548ab348ef1873b9812e77ab340ba122162746fe
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-11 17:00:02 +0200

    feat: :sparkles: add language picker component for picking a language

--- src/lib/components/LanguagePicker.svelte
+++ src/lib/components/LanguagePicker.svelte
@@ -19,21 +19,18 @@
 	aria-label="View density">
 	{#each optionsArr as option}
 		<div
+			class:border-orange-500={$isChecked(option)}
 			class:i-openmoji-flag-united-kingdom={option === "en"}
 			class:i-openmoji-flag-sweden={option === "sv"}
 			class:i-openmoji-flag-japan={option === "ja"}
-			class="flex h-12 w-12 items-center gap-3">
+			class="flex h-12 w-12 items-center gap-3 border-solid border-1">
 			<button
 				use:melt={$item(option)}
 				on:click={() => ($locale = option)}
 				class="grid h-12 w-12 cursor-default place-items-center rounded-full
       shadow-sm"
 				id={option}
-				aria-labelledby="{option}-label">
-				{#if $isChecked(option)}
-					<div class="h-[30px] w-[46px] rounded-sm border-2 border-orange-500" />
-				{/if}
-			</button>
+				aria-labelledby="{option}-label" />
 		</div>
 	{/each}
 </div>
