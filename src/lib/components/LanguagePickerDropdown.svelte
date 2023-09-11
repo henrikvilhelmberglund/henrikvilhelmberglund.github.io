096eac2d1f4bb90735b52a5a12c17de8177767cc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-11 17:00:28 +0200

    feat: :sparkles: add melt-ui dropdowns for language picker and theme picker components

---
+++ src/lib/components/LanguagePickerDropdown.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { createDropdownMenu, melt } from "@melt-ui/svelte";
+	import { fly } from "svelte/transition";
+	import { t, locale, locales } from "$lib/i18n/i18n";
+
+	const optionsArr = ["en", "sv", "ja"];
+
+	const {
+		elements: { menu, item, trigger, arrow },
+		states: { open },
+	} = createDropdownMenu();
+</script>
+
+<button
+	class:i-openmoji-flag-united-kingdom={$locale === "en"}
+	class:i-openmoji-flag-sweden={$locale === "sv"}
+	class:i-openmoji-flag-japan={$locale === "ja"}
+	class="mt-3 h-[40px] w-[40px]"
+	use:melt={$trigger} />
+{#if $open}
+	<div class="" use:melt={$menu} transition:fly={{ duration: 150, y: -10 }}>
+		<div
+			class="bg-primary-300 dark-bg-primary-800 flex flex-col gap-1 rounded-md rounded-t-none p-2">
+			{#each optionsArr as option}
+				<div class="flex h-12 w-12 items-center gap-3">
+					<button
+						class:i-openmoji-flag-united-kingdom={option === "en"}
+						class:i-openmoji-flag-sweden={option === "sv"}
+						class:i-openmoji-flag-japan={option === "ja"}
+						on:click={() => ($locale = option)}
+						class="grid h-12 w-12 cursor-default place-items-center rounded-full
+        shadow-sm"
+						id={option}
+						aria-labelledby="{option}-label" />
+				</div>
+			{/each}
+		</div>
+	</div>
+{/if}
---
+++ src/lib/components/theme/ThemeDropdown.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { createDropdownMenu, melt } from "@melt-ui/svelte";
+	import { color } from "$lib/stores";
+	import { themeColors } from "./theme";
+	import { fly } from "svelte/transition";
+	import DarkModeToggle from "$lib/components/theme/DarkModeToggle.svelte";
+
+	let selectedTheme;
+
+	$: {
+		if (selectedTheme) {
+			themeColors.forEach((theme) => {
+				document.documentElement.classList.remove(theme);
+			});
+			$color = selectedTheme;
+			document.documentElement.classList.add(selectedTheme);
+		}
+	}
+
+	const {
+		elements: { menu, item, trigger, arrow },
+		states: { open },
+	} = createDropdownMenu();
+</script>
+
+<button class="i-carbon:color-palette !bg-primary-900 mt-3 h-[40px] w-[40px]" use:melt={$trigger} />
+{#if $open}
+	<div class="!left-0" use:melt={$menu} transition:fly={{ duration: 150, y: -10 }}>
+		<div class="bg-primary-300 dark-bg-primary-800 flex flex-col gap-1 p-2 rounded-md rounded-t-none">
+			{#each themeColors as themeColor}
+				<button
+					on:click={() => (selectedTheme = themeColor)}
+					class="item h-10 w-10 rounded p-2 text-transparent md:p-2 bg-{themeColor}-500" />
+			{/each}
+			<DarkModeToggle />
+		</div>
+	</div>
+{/if}
