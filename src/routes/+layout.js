f9fd7b0ddbfc1a20e7c1b57df2188c513ef071d6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-11 17:00:58 +0200

    fix: :bug: avoid i18n resetting whenever preloading links by checking if $locale exists

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,12 +1,14 @@
 export const prerender = true;
+// export const ssr = false;
 
-import { loadTranslations } from "$lib/i18n/i18n";
+import { loadTranslations, locale } from "$lib/i18n/i18n";
+import { get } from "svelte/store";
 
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ url }) => {
 	const { pathname } = url;
 
-	const initLocale = "en"; // get from cookie, user session, ...
+	const initLocale = get(locale) ? get(locale) : "en"; // get from cookie, user session, ...
 
 	await loadTranslations(initLocale, pathname); // keep this just before the `return`
 
