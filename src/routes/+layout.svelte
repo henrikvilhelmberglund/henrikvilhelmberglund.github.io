113ca5bf9576f67be37f0d79f3bd47132ab463cd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-11 17:01:47 +0200

    feat: :sparkles: add layout with nav, language/theme pickers and i18n
    wraps entire page in a <main> tag also

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,55 @@
 <script>
+	import { browser } from "$app/environment";
+	import { page } from "$app/stores";
 	import LanguagePicker from "$lib/components/LanguagePicker.svelte";
+	import ThemeDropdown from "$lib/components/theme/ThemeDropdown.svelte";
+	import DarkModeToggle from "$lib/components/theme/DarkModeToggle.svelte";
+	import ThemeSwitcher from "$lib/components/theme/ThemeSwitcher.svelte";
 	import "@unocss/reset/tailwind.css";
+	import LanguagePickerDropdown from "$lib/components/LanguagePickerDropdown.svelte";
+	import { t, locale } from "$lib/i18n/i18n";
 
 	const routes = [
-		{ display: "Home", url: "/" },
-		{ display: "Projects", url: "/projects" },
+		{ display: "home", url: "/" },
+		{ display: "projects", url: "/projects" },
 	];
+	let currentPage;
+
+	$: if (browser) {
+		currentPage = $page.route;
+	}
+
+	// $: console.log($locale);
 </script>
 
-<nav
-	class="shadow-primary-200 dark:shadow-primary-800 relative bg-white dark:bg-primary-950 flex h-14 w-screen max-w-full justify-center items-center gap-12 shadow-lg">
-	{#each routes as { url, display }}
-		<a class="hover:bg-primary-200 dark:hover:bg-primary-900 bg-white dark:bg-primary-950 text-black dark:text-white rounded bg-white p-2 text-3xl" href={url}>{display}</a>
-	{/each}
-</nav>
-<nav class="absolute right-0 top-0 max-w-full justify-end">
-	<LanguagePicker />
-</nav>
-<slot />
+<main
+	class="dark:bg-primary-950 bg-primary-100 font-quicksand flex min-h-screen w-screen max-w-full flex-col items-center">
+	<header class="flex">
+		<nav
+			class="shadow-primary-200 dark:shadow-primary-800 dark:bg-primary-950 relative flex h-14 w-screen max-w-full items-center justify-center gap-12 bg-white shadow-lg">
+			{#each routes as { url, display }}
+				<a
+					class:shadow-inner={url === currentPage?.id}
+					class="hover:bg-primary-200 dark:hover:bg-primary-900 dark:bg-primary-950 rounded bg-white bg-white p-2 text-3xl text-black dark:text-white"
+					href={url}>{$t(`nav.${display}`)}</a>
+			{/each}
+		</nav>
+		<aside class="absolute left-0 top-2 hidden lg:block">
+			<ThemeSwitcher />
+			<DarkModeToggle />
+		</aside>
+		<aside class="absolute md:hidden">
+			<ThemeDropdown />
+		</aside>
+		<aside class="absolute right-0 top-0 hidden max-w-full justify-end md:block">
+			<LanguagePicker />
+		</aside>
+		<aside class="absolute right-0 top-0 max-w-full justify-end md:hidden">
+			<LanguagePickerDropdown />
+		</aside>
+	</header>
+	<slot />
+</main>
 
-<style></style>
+<style>
+</style>
