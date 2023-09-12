e0266b0f4e9fd40fb09da1825de7be738e39dd07
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-12 17:31:31 +0200

    feat: :sparkles: add Socials component

---
+++ src/lib/components/Socials.svelte
@@ -0,0 +1,27 @@
+<script>
+	const socials = [
+		{ name: "Github", link: "https://github.com/henrikvilhelmberglund" },
+		{
+			name: "LinkedIn",
+			link: "https://www.linkedin.com/in/henrikvilhelmberglund/",
+		},
+		{ name: "Twitter", link: "https://twitter.com/henrikvberglund" },
+	];
+</script>
+
+<section class="right-0 top-14 absolute self-baseline gap-3 rounded-t-0 flex rounded-md bg-white">
+	<h2 class="hidden text-center text-2xl">Socials</h2>
+	{#each socials as { link, name, icon }}
+		<a target="_blank" href={link}>
+			<article
+				class:i-mdi-twitter={name === "Twitter"}
+				class:!text-blue-400={name === "Twitter"}
+				class:i-mdi-github={name === "Github"}
+				class:i-mdi-linkedin={name === "LinkedIn"}
+				class:!text-blue-600={name === "LinkedIn"}
+				class="h-12 w-12 text-black" /></a>
+	{/each}
+</section>
+
+<style>
+</style>
