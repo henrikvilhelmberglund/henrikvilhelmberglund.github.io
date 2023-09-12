84d2e5848e2ed30371c40ebb06db79df5b55fe0e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-12 18:19:33 +0200

    feat: :sparkles: initialize with user preferred locale
    doesn't really work for SSR though

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,13 +2,15 @@ export const prerender = true;
 // export const ssr = false;
 
 import { loadTranslations, locale } from "$lib/i18n/i18n";
+import { userPreferredLocale } from "$lib/stores";
 import { get } from "svelte/store";
 
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ url }) => {
 	const { pathname } = url;
 
-	const initLocale = get(locale) ? get(locale) : "en"; // get from cookie, user session, ...
+	// TODO this will always give English as a default when SSRed, not sure if fixable
+	const initLocale = get(locale) ? get(locale) : get(userPreferredLocale); // get from cookie, user session, ...
 
 	await loadTranslations(initLocale, pathname); // keep this just before the `return`
 
