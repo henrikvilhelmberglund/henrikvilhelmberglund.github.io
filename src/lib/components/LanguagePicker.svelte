a4f377b6cffc383ae17c203381c2a703a5aedac5
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-17 00:41:21 +0200

    fix: :lipstick: improve styling

--- src/lib/components/LanguagePicker.svelte
+++ src/lib/components/LanguagePicker.svelte
@@ -19,15 +19,16 @@
 
 <div
 	use:melt={$root}
-	class="flex flex-col gap-3 data-[orientation=horizontal]:flex-row"
+	class="flex flex-col mt-2 gap-3 data-[orientation=horizontal]:flex-row"
 	aria-label="View density">
 	{#each optionsArr as option}
 		<div
-			class:border-orange-500={$isChecked(option)}
+			class:outline-orange-500={$isChecked(option)}
+			class:outline-solid={$isChecked(option)}
 			class:i-openmoji-flag-united-kingdom={option === "en"}
 			class:i-openmoji-flag-sweden={option === "sv"}
 			class:i-openmoji-flag-japan={option === "ja"}
-			class="border-1 flex h-12 w-12 items-center gap-3 border-solid">
+			class="outline-2 flex h-8 w-12 bg-cover bg-center items-center gap-3 -outline-offset-3">
 			<button
 				use:melt={$item(option)}
 				on:click={() => {
@@ -36,8 +37,8 @@
 					// ? this gives an error but locale is always a store, probably TS issue
 					$locale = option;
 				}}
-				class="grid h-12 w-12 cursor-default place-items-center rounded-full
-      shadow-sm"
+				class="grid h-8 w-12 cursor-default place-items-center rounded-full
+      "
 				id={option}
 				aria-labelledby="{option}-label" />
 		</div>
