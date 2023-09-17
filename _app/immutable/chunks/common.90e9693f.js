ae75775656691c1340dfd5df13fcfc0df95516bd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-17 17:49:10 +0200

    Updates

---
+++ _app/immutable/chunks/common.90e9693f.js
@@ -0,0 +1,1 @@
+const t="ベルグルンド・ヘンリックのポートフォリオサイトへようこそ！",n="初めまして！ベルグルンド・ヘンリックと申します。スウェーデンのNackademinにてフロンドエンド開発を勉強中です。SvelteとUnoCSS・Tailwindが大好きで、アクセシビリティで使いやすい且つ使い勝手が良いサイトを作るのも大好きです。",o="このサイトの目的は自分が作ったサイトをリンクしてアピールすることです。是非ご覧ください！",e="ベルグルンド・ヘンリックのポートフォリオサイト",i={greeting:t,introduction:n,purpose:o,SEOtitle:e};export{e as SEOtitle,i as default,t as greeting,n as introduction,o as purpose};
