2da4244ea1ee1465ff766e10df9b397086fa6b15
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-02 17:32:45 +0200

    feat: use inlang and fix hydration issues (from p tag inside p tag)

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,22 +1,27 @@
 <script>
 	import H1 from "$lib/components/H1.svelte";
 	import WhiteTextBox from "$lib/components/WhiteTextBox.svelte";
-	import { t } from "$lib/i18n/i18n";
 	import DOMPurify from "isomorphic-dompurify";
 	import { marked } from "marked";
+  
+  import * as m from "$lib/paraglide/messages.js";
+	import { languageTag } from "$lib/paraglide/runtime.js";
 
 	export let data;
 	$: projects = data.projects;
 </script>
 
 <svelte:head>
-	<title>{$t("projects.SEOtitle")}</title>
+	<title>{m.projects_SEOtitle()}</title>
+  <meta name="description" content={m.projects_SEOdescription()} />
 </svelte:head>
 
 <section class="flex w-full flex-col items-center pt-20">
-	<H1>{@html DOMPurify.sanitize(marked.parse($t("projects.title")))}</H1>
+	<H1>{@html DOMPurify.sanitize(marked.parse(m.projects_title()))}</H1>
 	<WhiteTextBox>
-		<p class="text-lg">{@html DOMPurify.sanitize(marked.parse($t("projects.introduction")))}</p>
+		<div class="text-lg">
+      {@html DOMPurify.sanitize(marked.parse(m.introduction()))}
+    </div>
 	</WhiteTextBox>
 	<div class="mt-8 flex w-full flex-wrap md:w-[60vw] md:justify-center md:gap-8 md:pb-24">
 		{#each Object.entries(projects) as [key, { url, github, hostedOn }]}
@@ -28,24 +33,28 @@
 						<a class="" href={url}>{key}</a>
 					</h2>
 					<div
-						class="bg-primary-900/15 mask-bg-hero-diagonal-stripes dark:bg-primary-100/5 pointer-events-none absolute left-0 top-0 h-full w-full" />
+						class="bg-primary-900/15 mask-bg-hero-diagonal-stripes dark:bg-primary-100/5 pointer-events-none absolute left-0 top-0 h-full w-full"></div>
 				</div>
 				<div class="flex flex-1 flex-col gap-4 p-4">
-					<p class="markdown flex-1 break-words">
-						{@html DOMPurify.sanitize(marked.parse($t(`projects.${key}.desc`)))}
-					</p>
+					<div class="markdown flex-1 break-words">
+            {#if key}
+            <!-- TODO fix type -->
+            {@const text = m[`${key.replaceAll(".", "_").replaceAll("-", "_")}`]()}
+						{@html DOMPurify.sanitize(marked.parse(text))}
+            {/if}
+					</div>
 					<div class="md:mt-6">
 						<span
 							class="text-2xl align-bottom"
 							title={hostedOn}
 							class:i-devicon-vercel-wordmark={hostedOn === "Vercel"}
 							class:i-devicon-cloudflare-wordmark={hostedOn === "Cloudflare Pages"}
-							class:i-devicon-github-wordmark={hostedOn === "Github Pages"} />
+							class:i-devicon-github-wordmark={hostedOn === "Github Pages"}></span>
 						<a class="break-words text-blue-600 dark:text-blue-300" href={url}>
 							{url}</a>
 					</div>
 					<div>
-						<span class="i-devicon-github text-2xl align-bottom" />
+						<span class="i-devicon-github text-2xl align-bottom"></span>
 						<a class="break-words  text-blue-600 dark:text-blue-300" href={github}>
 							{github}</a>
 					</div>
