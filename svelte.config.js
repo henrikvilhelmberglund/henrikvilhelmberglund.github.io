5d73672cd7b1ac08449101cda51bcfab0a6bb11b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-02 17:29:31 +0200

    fix: remove old stuff

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,11 @@
 // import preprocess from "svelte-preprocess";
 // import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-import { preprocessMeltUI } from "@melt-ui/pp";
-import sequence from "svelte-sequential-preprocessor";
 
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
 		adapter: adapter(),
 	},
