786ff3caf2bb6b86d652135145da6065be75cb4b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-02 17:31:55 +0200

    feat: update Socials.svelte

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -21,7 +21,7 @@
 				class:i-mdi-github={name === "Github"}
 				class:i-mdi-linkedin={name === "LinkedIn"}
 				class:!text-blue-600={name === "LinkedIn"}
-				class="h-12 w-12 text-black dark:text-slate-200" /></a>
+				class="h-12 w-12 text-black dark:text-slate-200"></article></a>
 	{/each}
 </section>
 
