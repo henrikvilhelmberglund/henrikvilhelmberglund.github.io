bd64f1a92fdc826985d0cf3669fd66651d51d498
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-07-05 14:51:06 +0200

    fix: use svelte preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 // use adapter-auto for zero config deployment to Vercel, adapter-static for Github Pages
 // import adapter from '@sveltejs/adapter-auto';
 // import preprocess from "svelte-preprocess";
-// import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
 	},
